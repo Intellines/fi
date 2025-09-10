5fde6372e87db2cc379dce64a2208784f502d33a
nikitairs <stlrvnk@gmail.com>
2025-09-10 22:29:19 +0300

    add basic secrets config

---
+++ src/config.py
@@ -0,0 +1,14 @@
+from typing import ClassVar
+from pydantic_settings import SettingsConfigDict, BaseSettings
+
+
+class Config(BaseSettings):
+    VERSION: str = "0.0.1"
+    ENV: str
+    X_API_KEY: str
+    DATABASE_URL: str
+
+    model_config: ClassVar[SettingsConfigDict] = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
+
+
+config: Config = Config()  # type: ignore
